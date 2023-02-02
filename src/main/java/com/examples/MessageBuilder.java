44de4c0fd574bf44a8f99cbbf74f9f0d1711be18
erdoganforesight <89521820+erdoganforesight@users.noreply.github.com>
2023-02-02 16:38:18 +0300

    Update MessageBuilder.java

--- src/main/java/com/examples/MessageBuilder.java
+++ src/main/java/com/examples/MessageBuilder.java
@@ -6,4 +6,12 @@ public class MessageBuilder {
     public static String concat(String... messages){
         return String.join(" ", messages);
     }
+    
+    public static String concat2(String... messages){
+        return String.join(" ", messages);
+    }
+    
+    public static String concat3(String... messages){
+        return String.join(" ", messages);
+    }
 }
